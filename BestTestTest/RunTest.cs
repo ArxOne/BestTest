deb3fe1ad6588b37fd62afca2347cd15ecd2d905
Pascal Craponne <pascal@craponne.fr>
2018-02-10 00:25:30 +0100

    Muting tests

--- BestTestTest/RunTest.cs
+++ BestTestTest/RunTest.cs
@@ -4,6 +4,7 @@
 
 namespace BestTestTest
 {
+    using System.IO;
     using System.Linq;
     using BestTest.Test;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -18,7 +19,7 @@ namespace BestTestTest
             var testEngine = new TestEngine();
             var worseTest = typeof(WorseTest.SimpleTest);
             var testParameters = new TestParameters { AssemblyPaths = { worseTest.Assembly.Location } };
-            var results = testEngine.Test(testParameters);
+            var results = testEngine.Test(testParameters, new ConsoleWriter(new StringWriter()));
             Assert.IsTrue(results.Any(r => r.Description.MethodName == nameof(SimpleTest.SucceedingTest) && r.ResultCode == ResultCode.Success));
             Assert.IsTrue(results.Any(r => r.Description.MethodName == nameof(SimpleTest.InconclusiveTest) && r.ResultCode == ResultCode.Inconclusive));
             Assert.IsTrue(results.Any(r => r.Description.MethodName == nameof(SimpleTest.FailingTest) && r.ResultCode == ResultCode.Failure));
