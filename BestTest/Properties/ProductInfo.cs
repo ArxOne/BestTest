4b156c021ea222d486644d3d4544a8fda7b30b45
Pascal Craponne <pascal@craponne.fr>
2018-02-10 14:07:48 +0100

    Renamed ProductInfo to ProductInfo.tt

--- BestTest/Properties/ProductInfo.cs
+++ BestTest/Properties/ProductInfo.cs
@@ -3,8 +3,8 @@
 // https://github.com/ArxOne/BestTest
 // MIT license, blah blah blah.
 
-// This file is generated from ProductInfo.tt and ../../ProductInfo,
-// so you'd better change those above if you want some results
+// This file is generated from ProductInfo.tt and ../../ProductInfo.tt,
+// so you'd better change those above if you want some lasting results
 
 using System.Reflection;
 using System.Resources;
@@ -12,7 +12,7 @@ using System.Resources;
 [assembly: AssemblyDescription("Run unit tests (MSTests format)")]
 [assembly: AssemblyCompany("Arx One")]
 [assembly: AssemblyProduct("BestTest")]
-[assembly: AssemblyCopyright("MIT license https://opensource.org/licenses/MIT")]
+[assembly: AssemblyCopyright("MIT license MIT (https://github.com/ArxOne/BestTest/blob/master/LICENSE)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
