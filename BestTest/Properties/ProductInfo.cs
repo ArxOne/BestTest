678bc8d44a218f7d6a00ba38b3f72ae8bce17f74
Pascal Craponne <pascal@craponne.fr>
2018-02-10 14:18:06 +0100

    Simplified versioning

--- BestTest/Properties/ProductInfo.cs
+++ BestTest/Properties/ProductInfo.cs
@@ -17,5 +17,5 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("0")]
-[assembly: AssemblyFileVersion("0.2.1")] // 
+[assembly: AssemblyVersion("0.2.1")]
+[assembly: AssemblyFileVersion("0.2.1")]
